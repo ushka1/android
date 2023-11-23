8f04d88b571290e915c43890dca1b0779308b183
ushka1 <dawid.prajzner01@gmail.com>
2023-11-23 10:08:49 +0100

    add todolist basic functionalities

--- exercise4-todo-list/TodoList/app/src/main/java/com/example/todolist/data/TaskItem.kt
+++ exercise4-todo-list/TodoList/app/src/main/java/com/example/todolist/data/TaskItem.kt
@@ -3,13 +3,14 @@ package com.example.todolist.data
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.UUID
 
 data class TaskItem(
-    val id: String,
-    val name: String,
-    val description: String,
-    val createdAt: Date,
-    val completed: Boolean
+    var name: String,
+    var description: String,
+    var createdAt: Date,
+    var completed: Boolean,
+    var id: UUID = UUID.randomUUID(),
 ) {
     fun getFormattedDate(): String {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
--- exercise4-todo-list/TodoList/app/src/main/java/com/example/todolist/ui/TaskListViewModel.kt
+++ exercise4-todo-list/TodoList/app/src/main/java/com/example/todolist/ui/TaskListViewModel.kt
@@ -1,10 +1,38 @@
 package com.example.todolist.ui
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.todolist.data.TaskItem
 import com.example.todolist.data.TaskRepository
+import java.util.UUID
 
-class TaskListViewModel(
-    private val taskRepository: TaskRepository
-) {
+class TaskListViewModel : ViewModel() {
 
+    private val taskList = MutableLiveData<List<TaskItem>>()
+    private val taskRepository = TaskRepository()
+
+    init {
+        taskList.value = taskRepository.getAllTasks()
+    }
+
+    fun addTask(task: TaskItem) {
+        taskRepository.addTask(task)
+        taskList.postValue(taskRepository.getAllTasks())
+    }
+
+    fun updateTask(id: UUID, name: String, description: String) {
+        val list = taskList.value
+        val task = list!!.find { it.id == id }!!
+        task.name = name
+        task.description = description
+        taskList.postValue(list!!)
+    }
+
+    fun setCompleted(task: TaskItem) {
+        val list = taskList.value
+        val task = list!!.find { it.id == task.id }!!
+        task.completed = !task.completed
+        taskList.postValue(list!!)
+    }
 
 }
