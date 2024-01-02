4f0d384611aac56e89adaa6a2aae8391493e80c1
ushka1 <dawid.prajzner01@gmail.com>
2024-01-02 18:36:04 +0100

    add firebase

--- exercise8-authn/AuthN/app/src/main/java/com/example/authn/MainActivity.kt
+++ exercise8-authn/AuthN/app/src/main/java/com/example/authn/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.authn
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
     }
+
+    public override fun onStart() {
+        super.onStart()
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            print("User authenticated")
+        } else {
+            print("User not authenticated")
+        }
+    }
+
 }
