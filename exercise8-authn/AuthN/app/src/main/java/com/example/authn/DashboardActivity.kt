a52211edcb73a53c9343f4e517469afb12688e9a
unknown <david.prajzner01@gmail.com>
2024-01-06 19:49:25 +0100

    move to standard google oauth

--- exercise8-authn/AuthN/app/src/main/java/com/example/authn/DashboardActivity.kt
+++ exercise8-authn/AuthN/app/src/main/java/com/example/authn/DashboardActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +23,16 @@ class DashboardActivity : AppCompatActivity() {
         val emailTextView = findViewById<TextView>(R.id.text_email)
         emailTextView.text = user?.email ?: "-"
 
+        val providersTextView = findViewById<TextView>(R.id.text_providers)
+        var providersText = ""
+        if (user != null) {
+            for (p in user.providerData) {
+                providersText += p.providerId + ", "
+            }
+            providersText = providersText.dropLast(2)
+        }
+        providersTextView.text = providersText
+
         val logoutButton = findViewById<Button>(R.id.button_logout)
         logoutButton.setOnClickListener {
             logoutHandler()
@@ -29,11 +40,26 @@ class DashboardActivity : AppCompatActivity() {
     }
 
     private fun logoutHandler() {
-        auth.signOut()
-        
+        logoutFirebase()
+        logoutGoogle()
+
         val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun logoutFirebase() {
+        auth.signOut()
+    }
+
+    private fun logoutGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient.signOut()
     }
 
 }
--- exercise8-authn/AuthN/app/src/main/java/com/example/authn/LoginFragment.kt
+++ exercise8-authn/AuthN/app/src/main/java/com/example/authn/LoginFragment.kt
@@ -1,7 +1,6 @@
 package com.example.authn
 
-import android.content.Intent
-import android.content.IntentSender
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,12 +9,13 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
@@ -50,7 +50,6 @@ class LoginFragment : Fragment() {
         switchRegisterButton.setOnClickListener {
             switchRegisterHandler()
         }
-
 
         return view
     }
@@ -88,70 +87,48 @@ class LoginFragment : Fragment() {
             }
     }
 
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
-    private val REQ_ONE_TAP = 123 // any unique to activity
-
     private fun googleLoginHandler() {
-        oneTapClient = Identity.getSignInClient(requireActivity())
-        signInRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    .setServerClientId(getString(R.string.web_client_id))
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestEmail()
             .build()
 
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener(requireActivity()) { result ->
-                try {
-                    startIntentSenderForResult(
-                        result.pendingIntent.intentSender, REQ_ONE_TAP,
-                        null, 0, 0, 0, null
-                    )
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e("ABC", "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
-            }
-            .addOnFailureListener(requireActivity()) { e ->
-                e.localizedMessage?.let { Log.d("ABC", it) }
-            }
+        val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        val signInIntent = googleSignInClient.signInIntent
+        googleLauncher.launch(signInIntent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        when (requestCode) {
-            REQ_ONE_TAP -> {
-                try {
-                    val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
-                    val idToken = googleCredential.googleIdToken
-                    when {
-                        idToken != null -> {
-                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-                            auth.signInWithCredential(firebaseCredential)
-                                .addOnCompleteListener(requireActivity()) { task ->
-                                    if (task.isSuccessful) {
-                                        Log.d("ABC", "signInWithCredential:success")
-                                    } else {
-                                        Log.w("ABC", "signInWithCredential:failure", task.exception)
-                                    }
-                                }
-                        }
-
-                        else -> {
-                            Log.d("ABC", "No ID token!")
-                        }
-                    }
-                } catch (e: ApiException) {
-                    Log.d("ABC", "One tap dismissed.")
-                }
+    private val googleLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleGoogleResult(task)
             }
         }
 
-
+    private fun handleGoogleResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                auth.signInWithCredential(credential).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d("ABC", "signInWithGoogle:success")
+                    } else {
+                        Log.d("ABC", "signInWithGoogle:failure")
+                        Toast
+                            .makeText(
+                                requireActivity(),
+                                it.exception.toString(),
+                                Toast.LENGTH_SHORT
+                            )
+                            .show()
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(requireActivity(), task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun switchRegisterHandler() {
