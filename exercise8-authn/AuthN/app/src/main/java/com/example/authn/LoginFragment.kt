dcc897e2e3713ccf21081efd5dc61b166ea5099e
unknown <david.prajzner01@gmail.com>
2024-01-06 19:02:18 +0100

    integrate google one tap auth

--- exercise8-authn/AuthN/app/src/main/java/com/example/authn/LoginFragment.kt
+++ exercise8-authn/AuthN/app/src/main/java/com/example/authn/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.example.authn
 
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,8 +12,13 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class LoginFragment : Fragment() {
 
@@ -81,16 +88,70 @@ class LoginFragment : Fragment() {
             }
     }
 
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private val REQ_ONE_TAP = 123 // any unique to activity
+
     private fun googleLoginHandler() {
-        val signInRequest = BeginSignInRequest.builder()
+        oneTapClient = Identity.getSignInClient(requireActivity())
+        signInRequest = BeginSignInRequest.builder()
             .setGoogleIdTokenRequestOptions(
                 BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
                     .setSupported(true)
                     .setServerClientId(getString(R.string.web_client_id))
-                    .setFilterByAuthorizedAccounts(true)
+                    .setFilterByAuthorizedAccounts(false)
                     .build()
             )
             .build()
+
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(requireActivity()) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e("ABC", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(requireActivity()) { e ->
+                e.localizedMessage?.let { Log.d("ABC", it) }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = googleCredential.googleIdToken
+                    when {
+                        idToken != null -> {
+                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                            auth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(requireActivity()) { task ->
+                                    if (task.isSuccessful) {
+                                        Log.d("ABC", "signInWithCredential:success")
+                                    } else {
+                                        Log.w("ABC", "signInWithCredential:failure", task.exception)
+                                    }
+                                }
+                        }
+
+                        else -> {
+                            Log.d("ABC", "No ID token!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    Log.d("ABC", "One tap dismissed.")
+                }
+            }
+        }
+
+
     }
 
     private fun switchRegisterHandler() {
