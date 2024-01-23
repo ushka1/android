e8363c27d9dd734aac3c7ccacb5d24f733bf4911
ushka1 <david.prajzner01@gmail.com>
2024-01-24 00:13:24 +0100

    add api setup guide

---
+++ exercise9-payments/Api/README.md
@@ -0,0 +1,5 @@
+# Setup
+
+1. Install dependencies with `npm install`.
+1. Copy `.env.example` to `.env` and fill in the values.
+1. Run `npm start` to start the server.
--- exercise9-payments/Api/src/index.ts
+++ exercise9-payments/Api/src/index.ts
@@ -1,23 +1,47 @@
 import express from 'express';
 import { Stripe } from 'stripe';
 
-const port = 3000;
-const host = '0.0.0.0';
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-if (!stripeSecretKey) throw new Error('Stripe Secret Key not found');
+const PORT = 3000;
+const HOST = '0.0.0.0';
+const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY;
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+if (!STRIPE_PUBLIC_KEY || !STRIPE_SECRET_KEY)
+  throw new Error('Missing Stripe keys');
 
 const app = express();
-const stripe = new Stripe(stripeSecretKey);
+const stripe = new Stripe(STRIPE_SECRET_KEY);
 
 app.get('/', (req, res) => {
+  console.log('Server is up');
+
   res.send('The server is up!');
 });
 
-app.post('/connection_token', async (req, res) => {
-  const token = await stripe.terminal.connectionTokens.create();
-  res.json({ secret: token.secret });
+app.post('/payment-sheet', async (req, res) => {
+  console.log('Creating payment sheet');
+
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: '2020-03-02' },
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'eur',
+    customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+    publishableKey: STRIPE_PUBLIC_KEY,
+  });
 });
 
-app.listen(port, host, () => {
-  console.log(`Running on port ${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Running on port ${PORT}`);
 });
