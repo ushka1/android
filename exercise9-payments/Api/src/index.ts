2bee2559ab6650aff2eb0268a70e3cfae3447376
ushka1 <david.prajzner01@gmail.com>
2024-01-23 23:11:14 +0100

    integrate stripe into api

--- exercise9-payments/Api/src/index.ts
+++ exercise9-payments/Api/src/index.ts
@@ -1,13 +1,23 @@
 import express from 'express';
+import { Stripe } from 'stripe';
+
+const port = 3000;
+const host = '0.0.0.0';
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+if (!stripeSecretKey) throw new Error('Stripe Secret Key not found');
 
 const app = express();
-const port = 8080;
-const host = '0.0.0.0';
+const stripe = new Stripe(stripeSecretKey);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('The server is up!');
+});
+
+app.post('/connection_token', async (req, res) => {
+  const token = await stripe.terminal.connectionTokens.create();
+  res.json({ secret: token.secret });
 });
 
 app.listen(port, host, () => {
-  console.log(`App listening at port ${port}`);
+  console.log(`Running on port ${port}`);
 });
